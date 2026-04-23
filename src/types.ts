973688410569250c538c365e48f00507d9fbc7da
Ayoub Chrigui <ayoubchrigui@takiacademyteam.com>
2026-04-23 20:20:01 +0100

    added support for transient and singleton scopes

--- src/types.ts
+++ src/types.ts
@@ -3,3 +3,5 @@ export type Constructor<T = any> = new (...args: any[]) => T;
 export type AbstractConstructor<T = any> = abstract new (...args: any[]) => T;
 
 export type AnyConstructor<T = any> = Constructor<T> | AbstractConstructor<T>;
+
+export type Scope = "singleton" | "transient";
